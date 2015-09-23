0d59e126d1b8769cdb395b6f3bc4af5b3394ce5e
Vladislav <vladkolodka@gmail.com>
2015-09-23 19:03:36 +0300

    Считывание файла и вывод в консоль построчно

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+// первая программа на C#
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        private string file_in = "in.txt";
+        private string file_out = "out.txt";
+
+
+        static void Main(string[] args)
+        {
+            Program start = new Program();
+            Console.ReadKey();
+        }
+        Program()
+        {
+            string[] lines = File.ReadAllLines(file_in);
+
+            foreach (string line in lines) {
+                Console.WriteLine(line);
+            }        
+        }
+    }
+}
