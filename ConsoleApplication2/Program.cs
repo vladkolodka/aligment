24c572a033fe7dba395250670831004381762301
Vladislav <vladkolodka@gmail.com>
2015-09-23 20:07:03 +0300

    Разделение на строки

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -14,6 +14,9 @@ namespace ConsoleApplication2
         private string file_in = "in.txt";
         private string file_out = "out.txt";
 
+        private int N;
+        private string In;
+        private string Out;
 
         static void Main(string[] args)
         {
@@ -24,9 +27,56 @@ namespace ConsoleApplication2
         {
             string[] lines = File.ReadAllLines(file_in);
 
-            foreach (string line in lines) {
-                Console.WriteLine(line);
-            }        
+            N = int.Parse(lines[0]);
+            In = lines[1];
+
+            // формируем строку
+            createString();
+            Console.WriteLine(Out);              
+        }
+        private string getSymbols(int count, char symbol)
+        {
+            string symbols = "";
+
+            for (int i = 0; i < count; i++)
+                symbols += symbol;
+
+            return symbols;
+        }
+        private void createString()
+        {
+            // разбиваем строку на слова
+            string[] words = In.Split(' ');
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                // для хранения текущей строки (линии)
+                List<string> line = new List<string>();
+                int count = 0;
+
+                // корректор количества пробелов
+                while (count + words[i].Length + (line.Count > 1 ? line.Count : 1) - 1 < N)
+                {
+                    count += words[i].Length;
+                    // добавляем слово в строку
+                    line.Add(words[i]);
+
+                    i++;
+                    if (i == words.Length) break;
+                }
+
+                int fixer = line.Count <= 1 ? 2 : line.Count;
+                // получаем строку с необходимым клюичеством пробелов
+                string spaces = " ";
+
+                for(int j = 0; j < line.Count; j++)
+                {
+                    Out += line[j];
+                    if (j + 1 == line.Count) Out += '\n';
+                    else Out += spaces;
+                }
+                i--;
+            }
         }
     }
 }
